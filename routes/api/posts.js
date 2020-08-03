38845cb21b279c560a36efe03930b237cb4ce96b
cdelange <casey.delange@gmail.com>
2020-08-03 17:42:52 -0500

    Implemented Profile models and routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Posts route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+
+// @route   Post api/posts
+// @desc    Create a post
+// @access  Private
+router.post('/', [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const user = await User
+
+  }
+);
 
 module.exports = router;
